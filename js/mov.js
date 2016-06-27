c8036bb6a9bf017d53ae8134a109ae0b9f6f9b9d
Marta Chupil <mchupil@mail.ru>
2016-06-27 14:22:58 +0300

    upload

---
+++ js/mov.js
@@ -0,0 +1,12 @@
+var movies = [
+    {
+        "name": "Scarface",
+        "director": " Брайан де Пальма",
+        "year": 1983
+    },
+    {
+        "name": "Scarface",
+        "director": " Брайан де Пальма",
+        "year": 1983
+    }
+];
